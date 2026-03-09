e6b4f4df25ea5ce25573d3ba91c598e2c267e614
qtqueen817-spec <qtqueen817@gmail.com>
2026-03-09 18:34:14 -0500

    Update hello.py
    first script

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 # My first script in the New-me repo
 name = input("What is your name? ")
-print("Hello, " + name + "! Welcome to your new repository.")
+print
+("Hello, " + name + "! Welcome to your new repository.")
