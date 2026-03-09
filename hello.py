ca3530beafa233dfc4ddd6689fa06da621ca96a0
qtqueen817-spec <qtqueen817@gmail.com>
2026-03-09 18:49:39 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 # My first script in the New-me repo
 name = input("What is your name? ")
+current_time = datetime.datetime.now()
+
 print("Hello, " + name + "! Welcome to your new repository.")
+print("The current date and time is: " + str(current_time))
